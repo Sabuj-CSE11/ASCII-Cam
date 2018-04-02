5b6f103e4b2826aa7c9571775e44c64c2fe970f8
yearnath <nathaniel.yearwood@uoit.net>
2018-04-02 14:27:22 -0400

    Initial Commit

---
+++ ASCII Cam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ASCII Cam
+//
+//  Created by Nathaniel Yearwood on 2018-04-02.
+//  Copyright © 2018 Nathaniel Yearwood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
