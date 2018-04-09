231dc86188c0904c7d64e87868e223a206829dd1
yearnath <nathaniel.yearwood@uoit.net>
2018-04-08 23:52:26 -0400

    ASCII view works, need to adjust spacing and clean up

--- ASCII Cam/ViewController.swift
+++ ASCII Cam/ViewController.swift
@@ -19,8 +19,9 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     var photoOutput: AVCapturePhotoOutput?
     
     @IBOutlet weak var processedView: UIImageView!
+    @IBOutlet weak var asciiView: UILabel!
     
-//    var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
+    //    var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
     
     var values = ["$", "@", "%", "#", "*", "+", "=", "-", ":", ",", " "]
 
@@ -35,7 +36,7 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     }
     
     func setupCaptureSession(){
-        captureSession.sessionPreset = AVCaptureSession.Preset.photo
+        captureSession.sessionPreset = AVCaptureSession.Preset.low
     }
     
     func setupDevice(){
@@ -95,8 +96,17 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func grayscale(byteBuffer : UnsafeMutablePointer<UInt8>, index : Int) -> UInt8 {
+        let b = UInt8(round(Double(byteBuffer[index]) * 0.11))
+        let g = UInt8(round(Double(byteBuffer[index+1]) * 0.59))
+        let r = UInt8(round(Double(byteBuffer[index+2]) * 0.3))
+        
+        return b + g + r
+    }
 
     func captureOutput(_ captureOutput: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        var output = [String]()
         connection.videoOrientation = AVCaptureVideoOrientation.portrait // rotate
         
         let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)!
@@ -113,30 +123,36 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         let byteBuffer = baseAddress.assumingMemoryBound(to: UInt8.self)
         
         for j in 0..<height {
+            output.append("")
             for i in 0..<width {
                 let index = (j * width + i) * 4
                 
-                let b = UInt8(round(Double(byteBuffer[index]) * 0.11))
-                let g = UInt8(round(Double(byteBuffer[index+1]) * 0.59))
-                let r = UInt8(round(Double(byteBuffer[index+2]) * 0.3))
+                let gray = grayscale(byteBuffer : byteBuffer, index : index)
                 
-                let gray = b + g + r
+                output[j].append(values[Int(Double(gray)/25.5)])
                 
                 byteBuffer[index] = gray
                 byteBuffer[index+1] = gray
                 byteBuffer[index+2] = gray
             }
         }
+        print("\n\n", width, height)
+        
+        for x in [0,3,6,9,12] {
+            print(values[Int(Double(grayscale(byteBuffer: byteBuffer, index: x))/25.5)])
+        }
         
         let colorSpace = CGColorSpaceCreateDeviceRGB()
         let bitmapInfo = CGImageAlphaInfo.premultipliedFirst.rawValue | CGBitmapInfo.byteOrder32Little.rawValue
-//        let colorSpace = CGColorSpaceCreateDeviceGray()
-//        let bitmapInfo = CGImageAlphaInfo.premultipliedFirst.rawValue | CGBitmapInfo.byteOrder16Little.rawValue
         let newContext = CGContext(data: baseAddress, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo)
         if let context = newContext {
             let cameraFrame = context.makeImage()
             DispatchQueue.main.async {
-                self.processedView.image = UIImage(cgImage: cameraFrame!)
+                self.asciiView.text = ""
+//                self.processedView.image = UIImage(cgImage: cameraFrame!)
+                for x in 0...(height-1) {
+                    self.asciiView.text?.append((output[x] + "\n"))
+                }
             }
         }
         
